24a420c47108afcdbacee7a166a4b4ce2aabb878
Anes Boukhelkhal <anesboukhelkhal@gmail.com>
2025-09-27 20:07:35 -0700

    Commit 5 Anes Boukhelkhal's Programming Folder

--- file1.c
+++ file1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main () {
-    printf(" This is test file 1\n");
+    
+    printf("This is test file 1\n");
     
     return 0;
 
--- file2.c
+++ file2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main () {
+
     printf("This is test file 2!\n");
 
     return 0;
